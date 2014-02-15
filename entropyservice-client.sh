9560148e3d27c7b10022da2bdce15408e587391a
dizzykey <dizzykey@users.noreply.github.com>
2014-02-15 15:01:07 +0000

    Update entropyservice-client.sh
    Added a check that /etc/rdcentropyservice exists and is a FIFO else corrects it. 
    Start the ssh and feed the FIFO before we stir entropy using rngd incase FIFO is depleted which caused the whole thing to stop.

--- entropyservice-client.sh
+++ entropyservice-client.sh
@@ -3,10 +3,24 @@ USER="entropyservice"
 SERVER="yourserver.tld"
 FIFO="/etc/rdcentropyservice"
 
-echo -ne "Starting rngd to stir external entropy into local entropy pool..."
-rngd -r $FIFO --fill-watermark=90% --feed-interval=1 --rng-timeout=0 --random-step=256 &
-echo "done."
+# check for and correct $FIFO as a FIFO pipe
+
+if ! [ -p "$FIFO" ]; then
+        echo "$FIFO is not a FIFO or is missing"
+        if [ -e "$FIFO" ] ; then
+                rm "$FIFO"
+        fi
+        mknod "$FIFO" p && echo "mknod $FIFO created as a FIFO"
+else
+        echo "$FIFO exists and is a FIFO"
+fi
+
+# start filling $FIFO so it's not depleted when we stir entropy into /dev/random using rngd
 
 echo -ne "Connecting to remote entropy pool..."
-ssh $USER@$SERVER "cat /dev/random" > $FIFO &
-echo "done."
+ssh  $USER@$SERVER "cat /dev/random" > $FIFO &
+echo  "done."
+
+echo -ne "Starting rngd to stir external entropy into local entropy pool..."
+rngd -r $FIFO --fill-watermark=90% --feed-interval=1 --rng-timeout=0 --random-step=256 &
+echo  "done."
