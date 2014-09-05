9ac77332c7015ef60381a4195e1a7c8c9d2674bc
arussell <aaron@unadopted.co.uk>
2014-09-05 19:09:22 +0100

    Use apt-get instead of aptitude on Debian and Ubuntu

--- entropyservice-install.sh
+++ entropyservice-install.sh
@@ -2,7 +2,7 @@
 # original https://github.com/arussell/entropyservice
 
 if [ -f /etc/debian_version ]; then
-  aptitude install rng-tools -y
+  apt-get install rng-tools -y
 fi
 if [ -f /etc/redhat-release ]; then
   yum install rng-utils
